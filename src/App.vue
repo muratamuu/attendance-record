e9e4056092c21516181dc318caf5f3d77309f64e
Takashi, Murata <murata.muu@gmail.com>
2020-02-13 00:31:25 +0900

    ヘッダをコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-spacer></v-spacer>
-      <v-btn
-        target="_blank"
-        text
-      >
-        <v-icon>mdi-login</v-icon>
-        <span class="mr-2">ログイン</span>
-      </v-btn>
-    </v-app-bar>
-
+    <Header/>
     <v-content>
       <router-view/>
     </v-content>
   </v-app>
 </template>
+
+<script>
+import Header from "./components/Header";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <v-spacer></v-spacer>
+    <v-btn
+      target="_blank"
+      text
+    >
+      <v-icon>mdi-login</v-icon>
+      <span class="mr-2">ログイン</span>
+    </v-btn>
+  </v-app-bar>
+</template>
