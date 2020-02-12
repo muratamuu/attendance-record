75fdb70352e6df88cb1f43c20b42d436689fdfdf
Takashi.Murata <Takashi.Murata@example.com>
2020-02-12 19:57:24 +0900

    favicon変更 bootstrap-vue入れた とりあえずメニューっぽいものの入れ物

---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <button>テストメニュー</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
